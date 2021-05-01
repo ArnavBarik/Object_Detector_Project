727d178526737feb445bd4d521ff5dc499aa86b6
ArnavBarik <71958007+ArnavBarik@users.noreply.github.com>
2021-05-01 11:07:42 +0530

    Add files via upload

--- kitchenmain.js
+++ kitchenmain.js
@@ -1,10 +1,11 @@
 img="";
 status="";
+objects=[];
 function preload(){
-    img=loadImage('');
+    img=loadImage('Kitchen.jpg');
 }
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(600,390);
     canvas.center();
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML="Status : Detecting Objects";
@@ -19,4 +20,19 @@ function gotResult(error,results){
         console.log(error);
     }
     console.log(results);
+    objects=results;
+}
+function draw(){
+    image(img,0,0,600,390);
+    if(status !=""){
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status : Object Detected";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+         }
+}
 }
